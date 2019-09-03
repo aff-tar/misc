e4e06b090068055d3d90791ac23f7d28296b37b6
Ilia Lin <ilia.lin@gmail.com>
2019-09-03 11:45:25 +0300

    rclone.sh

--- rclone.sh
+++ rclone.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for FOLDER in "0_Lin Family docs" "Apartment" "Arduino" "Car" "CV_Ilia" "CV_Alyssa" "Genealogy" "Ilia Bills" "International_Trips" "Israel trips" "ORG" "PhoneRecord" "Ringtones" "RPG" "tech" "kernel" "work"
+for FOLDER in "0_Lin Family docs" "Android" "Apartment" "Arduino" "Bike Routes" "Car" "CV_Ilia" "CV_Alyssa" "DND" "Garmin" "Genealogy" "Ilia Bills" "Intel" "International_Trips" "Israel trips" "kernel" "Manuals" "MISC" "ORG" "PhoneRecord" "Ringtones" "RPG" "tech" "Warranty" "Work"
 do
 rclone dedupe --dedupe-mode rename "gdrive:$FOLDER"
 rclone sync "gdrive:$FOLDER" "dropbox:$FOLDER" -P
